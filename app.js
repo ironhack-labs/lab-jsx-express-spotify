71e135e2c39fe0453e104918ef8127ae6573bed3
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2020-11-13 17:05:53 +0100

    Interation 1 done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const erv = require("express-react-views");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const SpotifyWebApi = require("spotify-web-api-node");
 
 // require spotify-web-api-node package here:
 
@@ -22,6 +23,18 @@ app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
 // setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 
 // Our routes go here:
